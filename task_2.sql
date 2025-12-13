7673a774ec82e67b68c16223253fcb813cdb4e2c
Victor <davik4life@gmail.com>
2025-12-13 16:16:00 +0100

    Added the task.sql file with only tables

--- task_2.sql
+++ task_2.sql
@@ -48,20 +48,39 @@ Name of the file should be task_2.sql
 All SQL keywords should be in uppercase
 */
 
-
-# Create Books table
-cursor.execute("""CREATE TABLE IF NOT EXISTS Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id INT,
+    author_id INT FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-)""")
+)
 
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215),
+)
 
-# Create Authors table
-cursor.execute("""CREATE TABLE IF NOT EXISTS Authors (
-               author_id INT PRIMARY KEY,
-               name VARCHAR(130)
-)""")   
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+)
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    CONSTRAINT fk_order
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    CONSTRAINT fk_book
+        FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
